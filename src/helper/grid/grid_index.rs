c8ec957d07443e078c142d20757f57591f020bcf
Felix Mätzler <felixmaetzler@gmx.de>
2024-12-26 13:43:52 +0100

    added Position as GridIndex

--- src/helper/grid/grid_index.rs
+++ src/helper/grid/grid_index.rs
@@ -1,6 +1,6 @@
 use std::hash::Hash;
 
-use crate::helper::position::Direction8;
+use crate::helper::position::{Direction8, Position};
 
 use super::Grid;
 
@@ -11,6 +11,7 @@ where
 {
     fn to_flat_index(&self, grid: &impl Grid<T>) -> usize;
     fn to_coordinates(&self, grid: &impl Grid<T>) -> (usize, usize);
+    fn to_position(&self, grid: &impl Grid<T>) -> Position<usize>;
     fn dir(&self, dir: Direction8, grid: &impl Grid<T>) -> Option<(usize, usize)> {
         grid.get_dir8(*self, dir).map(|(x, _)| x)
     }
@@ -24,6 +25,13 @@ impl<T> GridIndex<T> for usize {
     fn to_coordinates(&self, grid: &impl Grid<T>) -> (usize, usize) {
         (self / grid.width(), self % grid.width())
     }
+    #[inline(always)]
+    fn to_position(&self, grid: &impl Grid<T>) -> Position<usize> {
+        Position {
+            x: self % grid.width(),
+            y: self / grid.width(),
+        }
+    }
 }
 impl<T> GridIndex<T> for (usize, usize) {
     #[inline(always)]
@@ -34,6 +42,29 @@ impl<T> GridIndex<T> for (usize, usize) {
     }
     #[inline(always)]
     fn to_coordinates(&self, _grid: &impl Grid<T>) -> (usize, usize) {
+        *self
+    }
+
+    fn to_position(&self, _: &impl Grid<T>) -> Position<usize> {
+        Position {
+            x: self.1,
+            y: self.0,
+        }
+    }
+}
+impl<T> GridIndex<T> for Position<usize> {
+    #[inline(always)]
+    fn to_flat_index(&self, grid: &impl Grid<T>) -> usize {
+        debug_assert!(self.y < grid.height());
+        debug_assert!(self.x < grid.width());
+        self.y * grid.width() + self.x
+    }
+    #[inline(always)]
+    fn to_coordinates(&self, _grid: &impl Grid<T>) -> (usize, usize) {
+        self.as_yx_tuple()
+    }
+
+    fn to_position(&self, _: &impl Grid<T>) -> Position<usize> {
         *self
     }
 }
