b3e1817716d7ba96bfcdb4b5e33f5e0a63cf8526
Felix Mätzler <felixmaetzler@gmx.de>
2025-09-04 21:47:43 +0200

    extended range

--- src/helper/range.rs
+++ src/helper/range.rs
@@ -1,21 +1,101 @@
 use core::{
     cmp::Ord,
-    ops::{Add, RangeInclusive, Sub},
+    ops::{Add, Range, RangeInclusive, Sub},
 };
 
-/// Extension trait for `RangeInclusive<T>` to provide set-like operations for integer types.
-pub trait Ext<T>
+/// Trait to normalize access to start/end of ranges
+pub trait Normalized<T> {
+    fn start_bound(&self) -> T;
+    fn end_bound(&self) -> T;
+    fn is_inclusive(&self) -> bool;
+}
+
+impl<T: Copy> Normalized<T> for Range<T> {
+    fn start_bound(&self) -> T {
+        self.start
+    }
+
+    fn end_bound(&self) -> T {
+        self.end
+    }
+
+    fn is_inclusive(&self) -> bool {
+        false
+    }
+}
+
+impl<T: Copy> Normalized<T> for RangeInclusive<T> {
+    fn start_bound(&self) -> T {
+        *self.start()
+    }
+
+    fn end_bound(&self) -> T {
+        *self.end()
+    }
+
+    fn is_inclusive(&self) -> bool {
+        true
+    }
+}
+
+/// Extension trait for set-like operations on ranges
+pub trait ExtRangeOps<T>: Normalized<T>
+where
+    T: Copy + Ord + Add<Output = T> + Sub<Output = T> + From<u8>,
+    Self: Sized,
+{
+    fn intersection(&self, other: &Self) -> Option<Self>;
+    fn union(&self, other: &Self) -> Option<Self>;
+    fn overlaps(&self, other: &Self) -> bool;
+    fn contains_range(&self, other: &Self) -> bool;
+    fn subtract(&self, other: &Self) -> Vec<Self>;
+}
+
+// --- Implementations ---
+
+impl<T> ExtRangeOps<T> for Range<T>
 where
     T: Copy + Ord + Add<Output = T> + Sub<Output = T> + From<u8>,
 {
-    fn intersection(&self, other: &Self) -> Option<RangeInclusive<T>>;
-    fn union(&self, other: &Self) -> Option<RangeInclusive<T>>;
-    fn overlaps(&self, other: &Self) -> bool;
-    fn contains_range(&self, other: &Self) -> bool;
-    fn subtract(&self, other: &Self) -> Vec<RangeInclusive<T>>;
+    fn intersection(&self, other: &Self) -> Option<Self> {
+        let start = self.start.max(other.start);
+        let end = self.end.min(other.end);
+        (start < end).then_some(start..end)
+    }
+
+    fn union(&self, other: &Self) -> Option<Self> {
+        (self.overlaps(other) || self.end == other.start || other.end == self.start)
+            .then(|| self.start.min(other.start)..self.end.max(other.end))
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        self.start < other.end && other.start < self.end
+    }
+
+    fn contains_range(&self, other: &Self) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    fn subtract(&self, other: &Self) -> Vec<Self> {
+        if !self.overlaps(other) {
+            return vec![self.clone()];
+        }
+
+        let mut result = Vec::new();
+
+        if self.start < other.start {
+            result.push(self.start..other.start);
+        }
+
+        if self.end > other.end {
+            result.push(other.end..self.end);
+        }
+
+        result
+    }
 }
 
-impl<T> Ext<T> for RangeInclusive<T>
+impl<T> ExtRangeOps<T> for RangeInclusive<T>
 where
     T: Copy + Ord + Add<Output = T> + Sub<Output = T> + From<u8>,
 {
@@ -46,54 +126,15 @@ where
         }
 
         let mut result = Vec::new();
-        let start = *self.start();
-        let end = *self.end();
 
-        // Left segment
-        if start < *other.start() {
-            result.push(start..=other.start().sub(T::from(1)));
+        if self.start() < other.start() {
+            result.push(*self.start()..=*other.start() - T::from(1));
         }
 
-        // Right segment
-        if end > *other.end() {
-            result.push(other.end().add(T::from(1))..=end);
+        if self.end() > other.end() {
+            result.push(*other.end() + T::from(1)..=*self.end());
         }
 
         result
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_u32_subtract() {
-        assert_eq!(
-            (1_u32..=10_u32).subtract(&(3_u32..=7_u32)),
-            vec![1_u32..=2_u32, 8_u32..=10_u32]
-        );
-        assert_eq!(
-            (1_u32..=5_u32).subtract(&(6_u32..=10_u32)),
-            vec![1_u32..=5_u32]
-        );
-        assert_eq!((1_u32..=5_u32).subtract(&(1_u32..=5_u32)), vec![]);
-        assert_eq!(
-            (1_u32..=5_u32).subtract(&(3_u32..=10_u32)),
-            vec![1_u32..=2_u32]
-        );
-        assert_eq!(
-            (3_u32..=10_u32).subtract(&(1_u32..=5_u32)),
-            vec![6_u32..=10_u32]
-        );
-    }
-
-    #[test]
-    fn test_i32_intersection() {
-        assert_eq!(
-            (-5_i32..=5_i32).intersection(&(0_i32..=10_i32)),
-            Some(0_i32..=5_i32)
-        );
-        assert_eq!((-5_i32..=-1_i32).intersection(&(0_i32..=10_i32)), None);
     }
 }
